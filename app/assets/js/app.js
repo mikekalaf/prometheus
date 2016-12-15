a9f274925e9c82600bfb1d16c59536891cafda0a
mburden <michael.burden@mlb.com>
2016-12-14 21:41:53 -0500

    Updating Prometheus

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -11637,15 +11637,20 @@ if (typeof jQuery === 'undefined') {
     this.environment.screen.height = $(window).height();
     this.environment.screen.width = $(window).width();
     $(window).on('resize', prometheus.adjustViewPort());
-    $('#prometheusSplash').on('click', function() {
-      $('#loader').fadeOut();
-      setTimeout(function(){ $('#prometheusSplash').addClass('fadeOut'); }, 500);
-    })
+  },
+  loadDefaultView: function() {
   },
   adjustViewPort: function() {
     this.environment.screen.height = $(window).height();
     this.environment.screen.width = $(window).width();
     if(this.environment.mobile) {$('body').attr('id', 'mobile-app');} else {$('body').attr('id','desktop-app');}
+    $('#prometheusSplash').on('click', prometheus.removeSlashScreen);
+  },
+  removeSlashScreen: function() {
+    $('#loader').fadeOut();
+    setTimeout(function(){ $('#prometheusSplash').addClass('fadeOut'); }, 200);
+    $(this).fadeOut('slow');
+    setTimeout(function(){ prometheus.loadDefaultView(); }, 500);
   }
 };
 
--- app/assets/js/prometheus.js
+++ app/assets/js/prometheus.js
@@ -7,15 +7,20 @@ var prometheus = {
     this.environment.screen.height = $(window).height();
     this.environment.screen.width = $(window).width();
     $(window).on('resize', prometheus.adjustViewPort());
-    $('#prometheusSplash').on('click', function() {
-      $('#loader').fadeOut();
-      setTimeout(function(){ $('#prometheusSplash').addClass('fadeOut'); }, 500);
-    })
+  },
+  loadDefaultView: function() {
   },
   adjustViewPort: function() {
     this.environment.screen.height = $(window).height();
     this.environment.screen.width = $(window).width();
     if(this.environment.mobile) {$('body').attr('id', 'mobile-app');} else {$('body').attr('id','desktop-app');}
+    $('#prometheusSplash').on('click', prometheus.removeSlashScreen);
+  },
+  removeSlashScreen: function() {
+    $('#loader').fadeOut();
+    setTimeout(function(){ $('#prometheusSplash').addClass('fadeOut'); }, 200);
+    $(this).fadeOut('slow');
+    setTimeout(function(){ prometheus.loadDefaultView(); }, 500);
   }
 };
 
