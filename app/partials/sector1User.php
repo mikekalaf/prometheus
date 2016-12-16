4ce7572b7441b97fbc88f0fbf33f5d641a97226c
mburden <michael.burden@mlb.com>
2016-12-16 16:31:52 -0500

    Grid animations finally

--- app/partials/sector1User.php
+++ app/partials/sector1User.php
@@ -1,9 +1,8 @@
 <div id="grindrUser" class="app-overlay-window">
   <div class="app-overylay-header">
-    <div class="app-overlay-title">User Profile</div>
+    <div class="app-overlay-title">Title</div>
     <div class="app-overlay-close" data-target="grindrUser"><i class="fa fa-times"></i></div>
   </div>
   <div class="app-overlay-body">
-      <p>User Data</p>
   </div>
 </div>
