5ab2f502298b43dd4779db49cfb1af8fa3d82485
mburden <michael.burden@mlb.com>
2016-12-16 19:02:28 -0500

    Grid animations finally

--- app/partials/sector1.php
+++ app/partials/sector1.php
@@ -2,7 +2,7 @@
 include('../includes/prometheus.php');
 
 $favorites = (isset($_GET['favorites']) ? $_GET['favorites'] : 'No');
-$limit = (isset($_GET['limit']) ? $_GET['limit'] : '100');
+$limit = (isset($_GET['limit']) ? $_GET['limit'] : '150');
 $page = (isset($_GET['page']) ? $_GET['page'] : '1');
 $url = "http://v9.ikioskcloudapps.com/shield/x-gene/sector1?favorites=".$favorites."&limit=".$limit."&page=".$page;
 $request_headers = array();
@@ -14,7 +14,8 @@ echo "<div class='appGrid'>";
 foreach($sectorData['data'] as $key => $user) {
   $user['date_modified'] = date('M d Y, g:ia', strtotime($user['date_modified']));
   $userData = json_encode($user);
-  echo "<div class='gridItem grindrUser overlayLink' data-target='grindrUser' data-grid-id='".$user['profile_id']."' style='background-image: url(".$user['profile_photo'].");'></div>";
+  //echo "<div class='gridItem grindrUser overlayLink' data-target='grindrUser' data-grid-id='".$user['profile_id']."' style='background-image: url(".$user['profile_photo'].");'></div>";
+  echo "<div class='gridItem grindrUser overlayLink' data-target='grindrUser' data-grid-id='".$user['profile_id']."'></div>";
   $ajaxScripts .= "prometheus.gridData['".$user['profile_id']."'] = ".$userData.";\r\n";
 }
 echo "</div>";
