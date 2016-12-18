9da806e4558f2fb3c897948331b501da8533fa7c
mburden <michael.burden@mlb.com>
2016-12-17 19:29:33 -0500

    Adding Media Viewer

---
+++ app/partials/mediaViewer.php
@@ -0,0 +1,13 @@
+<div id="junkMedia" class="app-overlay-window">
+  <div class="app-overylay-header">
+    <div class="app-overlay-title">Media Viewer</div>
+    <div class="app-overlay-close" data-target="junkMedia"><i class="fa fa-times"></i></div>
+  </div>
+  <div class="app-overlay-body">
+      <div id="photoViewer">
+        <img src="http://www.themarysue.com/wp-content/uploads/2015/07/Jean_Grey_Phoenix.jpg" />
+      </div>
+      <div id="videoViewer">
+      </div>
+  </div>
+</div>
