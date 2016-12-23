9be1cc8931f4b12b0756d9e41f8c9213c98dc9c7
mburden <michael.burden@mlb.com>
2016-12-23 01:31:32 -0500

    Upgrading Skynet

--- app/partials/scruffGrid.php
+++ app/partials/scruffGrid.php
@@ -2,6 +2,6 @@
 
 $scruffUserCount = count($scruffUsers['results']);
 foreach ($scruffUsers['results'] as $key => $user) {
-  echo "<div class='gridItem' style='background-image: url(".$user['thumbnail'].")'></div>";
+  echo "<div class='gridItem' style='background-image: url(".$user['thumbnail_url'].")'></div>";
 }
  ?>
