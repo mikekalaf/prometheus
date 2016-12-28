5395b3b88fe27103ae1c0649a5c14f339296f10b
mburden <michael.burden@mlb.com>
2016-12-28 16:09:55 -0500

    Updating Map

--- app/partials/map.php
+++ app/partials/map.php
@@ -89,7 +89,7 @@ var styledMapType = new google.maps.StyledMapType(
 var mapCenter = {lat: 40.748441, lng: -73.985664};
 setTimeout(function() {
   prometheus.googlemap = new google.maps.Map(document.getElementById('cerebroMap'), {
-   zoom: 5,
+   zoom: 3,
    center: mapCenter
   });
   prometheus.googlemap.mapTypes.set('styled_map', styledMapType);
