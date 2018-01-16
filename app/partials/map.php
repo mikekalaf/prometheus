cd9557bfef2dcae9bcf47089c22750d8645f2953
mburden <michael.burden@mlb.com>
2018-01-16 14:44:35 -0800

    Curl handler is messed up?

--- app/partials/map.php
+++ app/partials/map.php
@@ -4,7 +4,6 @@ include('../includes/skynet.php');
 $url = "http://v9.ikioskcloudapps.com/shield/cerebro/map";
 $request_headers = array();
 $fetchData = curl_handler($url, $request_headers, $blank, "GET");
-echo $fetchData;
 $sectorData = json_decode($fetchData ,true);
 
 //Cleanup the Duplicates
